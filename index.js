3303ba5dfb9229b770dd2c723a7a446f494188ae
Louis Suo <louis.suo@myob.com>
2016-11-10 15:34:42 +1100

    multiple routes

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const goodOptions = {
   }
 }
 
+function handler(req, res) {
+  res(req.params)
+}
+
 server.register({
   register: require('good'),
   options: goodOptions
@@ -31,11 +35,23 @@ server.register({
 
   server.route({
     method: 'GET',
-    path: '/{name}',
-    handler: (req, res) => {
-      res(`hello ${req.params.name}`)
-    }
+    path: '/{name*}',
+    handler: handler
   })
+
+  server.route({
+    method: 'GET',
+    path: '/files/{name*}',
+    handler: handler
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/jpg/{name}.jpg',
+    handler: handler
+  })
+
+
   server.start(() => console.log(`started at ${server.info.uri}`))
 })
 
